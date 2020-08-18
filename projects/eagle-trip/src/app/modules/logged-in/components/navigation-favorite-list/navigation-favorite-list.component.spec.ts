f087dc0e496f2108ab5dd9dbb416f3d986dcc708
Nicolás Falla Ríos <nicolasfalla15@gmail.com>
2020-08-18 10:07:25 -0500

    add test for render items

--- projects/eagle-trip/src/app/modules/logged-in/components/navigation-favorite-list/navigation-favorite-list.component.spec.ts
+++ projects/eagle-trip/src/app/modules/logged-in/components/navigation-favorite-list/navigation-favorite-list.component.spec.ts
@@ -4,16 +4,51 @@ import { NavigationFavoriteListComponent } from './navigation-favorite-list.comp
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {NG_ENTITY_SERVICE_CONFIG} from '@datorama/akita-ng-entity-service';
 import {environment} from '@env/environment';
+import {UserFavoritesService} from '../../../../core/state/user-favorite/user-favorites.service';
+import {UserFavoritesStore} from '../../../../core/state/user-favorite/user-favorites.store';
+import {By} from '@angular/platform-browser';
+import {UserFavorite} from '../../../../core/state/user-favorite/user-favorite.model';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {CommonModule} from '@angular/common';
+import {UserFavoritesQuery} from '../../../../core/state/user-favorite/user-favorites.query';
+
+
+export function mockUserFavoritesStore(): UserFavorite {
+  return  {
+    address: 'test',
+    name: 'test',
+    id: 1,
+    longitude: 2332,
+    latitude: 322323
+  };
+}
+
+
+export class UserFavoriteStoreSub extends UserFavoritesStore {
+  constructor() {
+    super();
+  }
+}
+
 
 describe('NavigationFavoriteListComponent', () => {
   let component: NavigationFavoriteListComponent;
   let fixture: ComponentFixture<NavigationFavoriteListComponent>;
+  let userFavoritesService: UserFavoritesService;
+  let store: UserFavoritesStore;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NavigationFavoriteListComponent ],
-      imports: [HttpClientTestingModule],
+    return TestBed.configureTestingModule({
+      declarations: [NavigationFavoriteListComponent],
+      imports: [HttpClientTestingModule, MatListModule, MatIconModule, CommonModule],
       providers: [
+        UserFavoritesService,
+        {
+        UserFavoritesStore,
+          useValue: jasmine.createSpy('UserFavoritesStore', () => ['set'])
+        },
+        UserFavoritesQuery,
         {
           provide: NG_ENTITY_SERVICE_CONFIG,
           useValue: {
@@ -22,16 +57,25 @@ describe('NavigationFavoriteListComponent', () => {
         }
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavigationFavoriteListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    userFavoritesService = TestBed.inject(UserFavoritesService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display length 2', () => {
+    userFavoritesService.store.set([mockUserFavoritesStore(), mockUserFavoritesStore()]);
+    fixture.detectChanges();
+    const todos = fixture.debugElement.queryAll(By.css('mat-list-option'));
+    expect(todos.length).toEqual(2);
+  });
+
 });
